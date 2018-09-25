e9f4917533d77eabfff96b59750a3a8903962a92
Glitch (periodic-bag) <none>
2018-09-25 05:05:40 +0000

    🚏😇 Checkpoint ./package.json:1841575/164 ./server.js:1841575/2338 ./views/index.html:1841575/2187

--- server.js
+++ server.js
@@ -3,8 +3,12 @@
 
 // init project
 var express = require('express');
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var path = require('path');     //used for file path
+var fs = require('fs-extra'); 
 var app = express();
-
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -15,6 +19,24 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.route('/upload')
+    .post(function (req, res, next) {
+
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+             file.on('end', function() {
+                res.send({
+                 "filename":filename,
+                 "size": Object.size(file)
+                });
+              });
+        });
+    });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
